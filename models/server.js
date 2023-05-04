8fd94c5ac3fd8f1ae36717063599ffd29e723203
Karen Plata <karen.platahdz2223@gmail.com>
2023-05-04 08:38:25 -0600

    Configuracion Socket

--- models/server.js
+++ models/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
+const { createServer } = require('http');
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+        this.server = createServer( this.app );
+        this.io = require('socket.io')( this.server );
 
         this.paths = {
             auth:       '/api/auth',
@@ -28,6 +32,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // Sockets
+        this.sockets();
     }
 
     async conectarDB() {
@@ -66,8 +73,12 @@ class Server {
         
     }
 
+    sockets() {
+        this.io.on('connection', socketController );
+    }
+
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
